e659190fe15570e364474fded3e124787539893d
RenanLemes <renan_ll@hotmail.com>
2022-10-02 10:35:09 -0300

    Chores:arrumando os movimentos

--- Problemados8puzzles.py
+++ Problemados8puzzles.py
@@ -11,8 +11,8 @@ Estado_fin = [['1', '2', '3'],
                 ['7', '8', '_']]
 
 Estado_ini = [['5', '8', '7'],
-              ['4', '2', '3'], 
-              ['6', '1', '_']]
+              ['4', '_', '2'], 
+              ['3', '6', '1']]
 
 n = 0
 
@@ -40,23 +40,96 @@ def func_h(Est_i, Est_f):
 
     return h 
 
-def mov(Est, n):
-    n += 1
-    #
-    ''' 3 down
+def mov_9(Est, n):
+    ''' 
+        Est: estado da matriz
+        n: quantidade de movimentos
+    
+    '''
+
+    # moviemnto 1
     temp = Est[1][2]
     Est[1][2] = Est[2][2]
     Est[2][2] = temp 
     
-    '''
-
-    '''
+    # movimento 2
     temp = Est[2][1]
     Est[2][1] = Est[2][2]
     Est[2][2] = temp 
-    '''
 
-    return Est, n   
+    return Est, n    
+
+def mov_8(Est, n):
+    
+    # movimento 1
+    temp = Est[2][2]
+    Est[2][2] = Est[2][1]
+    Est[2][1] = temp
+    
+    # movimento 2
+    temp = Est[2][0]
+    Est[2][0] = Est[2][1]
+    Est[2][1] = temp 
+
+    # movimento 3
+    temp = Est[1][1]
+    Est[1][1] = Est[2][1]
+    Est[2][1] = temp
+
+    return Est, n
+
+def mov_7(Est, n):
+     # movimento 1
+    temp = Est[1][0]
+    Est[1][0] = Est[2][0]
+    Est[2][0] = temp 
+    
+    # movimento 2
+    temp = Est[2][1]
+    Est[2][1] = Est[2][0]
+    Est[2][0] = temp
+
+    return Est, n
+
+def mov_6(Est, n):
+    
+    # movimento 1
+    temp = Est[0][2]
+    Est[0][2] = Est[1][2]
+    Est[1][2] = temp
+    
+    # movimento 2
+    temp = Est[1][1]
+    Est[1][1] = Est[1][2]
+    Est[1][2] = temp
+
+    # movimento 3
+    temp = Est[2][2]
+    Est[2][2] = Est[1][2]
+    Est[1][2] = temp
+
+    return Est, n
+
+def mov_5(Est, n):
+    pass
+def mov_4(Est, n):
+    pass
+def mov_3(Est, n):
+    pass
+def mov_2(Est, n):
+    pass
+def mov_1(Est, n):
+    pass
+
+
+def mov(Est, n):
+    n += 1
+
+    
+    
+
+    return Est, n 
+      
 
 #print(func_h(Estado_ini,Estado_fin))
 
